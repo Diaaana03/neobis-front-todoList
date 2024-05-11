ac00f3f56f8247aae2df7ff206d95a6279793747
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2024-05-11 02:34:16 +0100

    changed css and added date and time

---
+++ index.js
@@ -0,0 +1,21 @@
+const data = new Date();
+
+const timeElapsed = Date.now();
+const today = new Date(timeElapsed);
+
+document.getElementById("date").innerHTML = today.toDateString();
+
+function time() {
+  const data = new Date();
+
+  let h = data.getHours();
+  let m = data.getMinutes();
+  let s = data.getSeconds();
+
+  if (h < 10) h = "0" + h;
+  if (m < 10) m = "0" + m;
+  if (s < 10) s = "0" + s;
+
+  document.getElementById("hour").innerHTML = h + ":" + m + ":" + s;
+  setTimeout("time()", 500);
+}
